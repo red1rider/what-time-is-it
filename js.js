c78f571fe35600cbdbbee1806b886de065cf67a5
Anastasiya <148859924+red1rider@users.noreply.github.com>
2025-06-14 18:52:22 +0300

    Delete js.js

--- js.js
+++
@@ -1,7 +0,0 @@
-function displayTime() {
-    let element = document.getElementById("clock"); 
-    let now = new Date(); 
-    element.innerHTML = now.toLocaleTimeString(); 
-    setTimeout(displayTime, 1000);
-  
-  window.onload = displayTime;
