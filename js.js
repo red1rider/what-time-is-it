211cd1c0bbae3f737ec02a03f8b5117c539a1254
Anastasiya <148859924+red1rider@users.noreply.github.com>
2025-06-14 19:04:26 +0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,8 @@
+function displayTime() {
+  let element = document.getElementById("clock"); 
+  let now = new Date(); 
+  element.innerHTML = now.toLocaleTimeString(); 
+  setTimeout(displayTime, 1000);
+}
+
+window.onload = displayTime;
